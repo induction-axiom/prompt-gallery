c3fb9c6e7a654ff3a15d6166d2959f1c4e555a4d
Jong Luo <1999luozhonghao@gmail.com>
2025-12-19 09:51:56 -0500

    style: Update primary button appearance to use a transparent background with a border and new hover effects.

--- src/components/templates/TemplateCard.jsx
+++ src/components/templates/TemplateCard.jsx
@@ -78,7 +78,7 @@ const TemplateCard = ({ template, onRun, onView, onEdit, onRemix, onDelete, onDe
             <div className="relative h-0 z-10 flex justify-center">
                 <button
                     onClick={onRun}
-                    className="absolute -top-5 flex items-center justify-center gap-2 px-6 py-2.5 bg-white dark:bg-gray-800 text-firebase-orange font-semibold rounded-full shadow-lg border border-gray-100 dark:border-gray-600 hover:bg-firebase-orange/5 hover:shadow-xl transform transition-all hover:-translate-y-0.5 active:translate-y-0 cursor-pointer"
+                    className="absolute -top-5 flex items-center justify-center gap-2 px-6 py-2.5 bg-white dark:bg-gray-800 border border-firebase-orange text-gray-900 dark:text-white hover:bg-firebase-orange hover:text-gray-900 dark:hover:text-gray-900 font-semibold rounded-full shadow-lg transform transition-all hover:-translate-y-0.5 active:translate-y-0 cursor-pointer"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
